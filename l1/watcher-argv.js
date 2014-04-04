8d7be5a4fc52354ff6342892a5c465df5cd9e887
Lucas <lucas.silva@agenciamilagro.com.br>
2014-04-04 10:06:26 -0300

    Update watcher-argv

--- l1/watcher-argv.js
+++ l1/watcher-argv.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
+
+// argv stands for Argument Vector, it’s an array containing node and the full path to the watcher-argv.js as its first two elements.
+// The third element (that is, at index 2) is target.txt, the name of our target file.
 const filename = process.argv[2];
 
 if (!filename) {
