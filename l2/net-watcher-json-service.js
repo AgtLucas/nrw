853c22050a8c426e501a56401099ecacefaed8d5
Lucas Churchill <lucas@agtlucas.com>
2014-04-05 15:16:18 -0300

    Fix typo

--- l2/net-watcher-json-service.js
+++ l2/net-watcher-json-service.js
@@ -40,6 +40,6 @@ if (!filename) {
   throw Error('No target filename was specified.');
 }
 
-server.listen(5342, function () {
+server.listen(5432, function () {
   console.log('Listening for subrscribers...');
 });
